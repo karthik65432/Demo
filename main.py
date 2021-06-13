aee17f4d9b730643f016a209efe510fb555dc466
karthik65432 <karthik65432@gmail.com>
2021-06-13 13:38:19 +0530

    OOPS

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+class Student:
+    School = 'Telusko'
+
+    def __init__(self, name, rollno,cpu):
+        self.name = name
+        self.rollno = rollno
+        self.lap = self.config(cpu)
+
+    class config:
+
+        def __init__(self, cpu='i5',ram='8gb'):
+            self.cpu = cpu
+            self.ram = ram
+
+
+s1= Student('Karthik',1245978,'i7')
+print(s1.lap.cpu)
+
+
 
